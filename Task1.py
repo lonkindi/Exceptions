fd33eb8101650e5197f043471351047217f3f22d
lonkindi <legions.ru@gmail.com>
2020-02-07 08:09:35 +0500

    Added files, edited Task1

--- Task1.py
+++ Task1.py
@@ -1,8 +1,13 @@
-parameters = list(map(str, input('Введите оператор и два положительных целых числа через пробел:>').split(sep=' ')))
-# assert parameters[0] in ['+', '-', '*', '/'], print('Введённый оператор не поддерживается, используйте + - * /')
-try:
-    parameters[1], parameters[2] = int(parameters[1]), int(parameters[2])
-except:
-    print("TypeError")
-
-print(parameters)
+result = 0
+parameters = list(
+    map(str, input('Введите оператор и два положительных целых числа через пробел (например, + 2 2):>').split(sep=' ')))
+parameters[1], parameters[2] = int(parameters[1]), int(parameters[2])
+if parameters[0] == '+':
+    result = parameters[1] + parameters[2]
+elif parameters[0] == '-':
+    result = parameters[1] - parameters[2]
+elif parameters[0] == '*':
+    result = parameters[1] * parameters[2]
+elif parameters[0] == '/':
+    result = parameters[1] / parameters[2]
+print(result)
---
+++ Task2.py
@@ -0,0 +1,22 @@
+result = 0
+parameters = list(map(str, input('Введите оператор и два положительных целых числа через пробел (например, + 2 2):>').split(sep=' ')))
+parameters[1], parameters[2] = int(parameters[1]), int(parameters[2])
+if parameters[0] == '+':
+    result = parameters[1] + parameters[2]
+elif parameters[0] == '-':
+    result = parameters[1] - parameters[2]
+elif parameters[0] == '*':
+    result = parameters[1] * parameters[2]
+elif parameters[0] == '/':
+    result = parameters[1] / parameters[2]
+# assert parameters[0] in ['+', '-', '*', '/'], print('Введённый оператор не поддерживается, используйте + - * /')
+# try:
+#     parameters[1] = int(parameters[1])
+# except ValueError:
+#     print("Первый операнд не является числом")
+# try:
+#     parameters[2] = int(parameters[2])
+# except ValueError:
+#     print("Второй операнд не является числом")
+
+print(result)

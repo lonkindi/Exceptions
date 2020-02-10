69c0b7fba5ababc18ddfa5e7bb0c409654a148a9
lonkindi <legions.ru@gmail.com>
2020-02-10 11:29:39 +0500

    Task 2 completed

--- Task2.py
+++ Task2.py
@@ -1,26 +1,28 @@
-result = 0
-parameters = list(map(str, input('Введите оператор и два положительных целых числа через пробел (например, + 2 2):>').split(sep=' ')))
+result = ''
+parameters = list(
+    map(str, input('Введите оператор и два положительных целых числа через пробел (например, + 2 2):>').split(sep=' ')))
 try:
-    assert parameters[0] in ['+', '-', '*', '/'], print('Введённый оператор не поддерживается, используйте + - * /')
-except:
-    pass
-parameters[1], parameters[2] = int(parameters[1]), int(parameters[2])
-if parameters[0] == '+':
-    result = parameters[1] + parameters[2]
-elif parameters[0] == '-':
-    result = parameters[1] - parameters[2]
-elif parameters[0] == '*':
-    result = parameters[1] * parameters[2]
-elif parameters[0] == '/':
-    result = parameters[1] / parameters[2]
-# assert parameters[0] in ['+', '-', '*', '/'], print('Введённый оператор не поддерживается, используйте + - * /')
-# try:
-#     parameters[1] = int(parameters[1])
-# except ValueError:
-#     print("Первый операнд не является числом")
-# try:
-#     parameters[2] = int(parameters[2])
-# except ValueError:
-#     print("Второй операнд не является числом")
-
-print(result)
+    assert parameters[0] in ['+', '-', '*', '/']
+    parameters[1] = int(parameters[1])
+    parameters[2] = int(parameters[2])
+    if parameters[0] == '+':
+        result = parameters[1] + parameters[2]
+    elif parameters[0] == '-':
+        result = parameters[1] - parameters[2]
+    elif parameters[0] == '*':
+        result = parameters[1] * parameters[2]
+    elif parameters[0] == '/':
+        result = parameters[1] / parameters[2]
+except AssertionError:
+    print('Введённый оператор не поддерживается, используйте "+" или "-" или "*" или "/"')
+except ZeroDivisionError:
+    print('Попытка разделить на ноль')
+except IndexError:
+    print('Введено недостаточное количество параметров операции для расчёта')
+except ValueError:
+    print(f'Один из операндов не является числом')
+else:
+    print(f'{parameters[1]} {parameters[0]} {parameters[2]} = {result}')
+finally:
+    if result == '':
+        print('Вычисление выполнить не удалось. Вы ввели некорректные данные.')
